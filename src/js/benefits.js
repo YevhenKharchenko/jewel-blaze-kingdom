1bd6c0a98c8567fdacc97856055af15e479bc5ed
YevhenKharchenko <evgenvalerych@gmail.com>
2025-04-09 23:02:02 +0300

    Add benefits, about and play sections

--- src/js/benefits.js
+++ src/js/benefits.js
@@ -1,42 +1,29 @@
 import Swiper from 'swiper';
+import { Autoplay } from 'swiper/modules';
 import 'swiper/css/bundle';
 
-const leftArrow = document.getElementById('leftArrow');
-const rightArrow = document.getElementById('rightArrow');
+Swiper.use([Autoplay]);
 
 const benefitsSwiper = new Swiper('.benefits-swiper-container', {
   direction: 'horizontal',
-  loop: false,
+  loop: true,
   grabCursor: true,
   slidesPerView: 1,
-  initialSlide: 0,
+  initialSlide: 3,
   spaceBetween: 22,
   grabCursor: true,
   allowTouchMove: true,
   speed: 500,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
   on: {
     init: () => {
       document
         .querySelector('.benefits-swiper-container')
         .classList.add('show');
     },
-    slideChange: () => {
-      updateArrows();
-    },
   },
-});
-
-function updateArrows() {
-  leftArrow.disabled = benefitsSwiper.isBeginning;
-  rightArrow.disabled = benefitsSwiper.isEnd;
-}
-
-updateArrows();
-
-leftArrow.addEventListener('click', () => {
-  benefitsSwiper.slidePrev();
-});
-
-rightArrow.addEventListener('click', () => {
-  benefitsSwiper.slideNext();
 });
